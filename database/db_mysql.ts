febefe6a01b52f7b307e9c46d0629bc7dd5aa6bb
comodoroCampos <comodoro1campos@gmail.com>
2022-05-27 17:31:04 -0400

    a

--- database/db_mysql.ts
+++ database/db_mysql.ts
@@ -1,10 +1,5 @@
 import {Sequelize}  from 'sequelize';
 
-
-//const db = new Sequelize('postgres://postgres:SerSaa***2021@192.168.61.38:5432/municipalidad')
-//const db = new Sequelize('mssql://sa:gemini491110!@192.168.1.22:1433/aep')
-
-
 const db = new Sequelize(
   process.env.DB_DATABASE ||'ccl50745_api',
   process.env.DB_USERNAME || 'ccl50745_api',
