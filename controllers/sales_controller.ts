fe6d85fd8f2cbe89999da1b2c790f71f97669a10
joaquin fernandez <47857926+jfernalv106@users.noreply.github.com>
2022-07-11 22:30:17 -0400

    s

--- controllers/sales_controller.ts
+++ controllers/sales_controller.ts
@@ -51,13 +51,10 @@ export const getSalesFecha = async (req: Request, res: Response) => {
 };
 export const getSalesGrafico = async (req: Request, res: Response) => {
 
-
-
   let query = ' SELECT ';
   query += ' SUM(sl.amount) AS total, ';
-  query += ' DATE_FORMAT(sl.created_at, "%m-%Y") AS fecha ';
-  query += ' FROM sales AS sl GROUP BY DATE_FORMAT(sl.created_at, "%m-%Y") ';
-
+  query += ' DATE_FORMAT(sl.created_at, "%d-%m-%Y") AS fecha ';
+  query += ' FROM sales AS sl GROUP BY DATE_FORMAT(sl.created_at, "%d-%m-%Y") ';
 
   try {
     const sales = await SaleEntity.sequelize?.query(query, {
